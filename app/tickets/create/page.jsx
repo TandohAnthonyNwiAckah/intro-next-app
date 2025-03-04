23fa68f0c8000e7678e8ddce0b7b8ef18e81e0b7
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2025-03-04 14:24:26 +0000

    commit client form component

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import CreateForm from "./createForm";
+
+const CreateTicket = () => {
+  return (
+    <main>
+      <h2 className="text-primary text-center">Add a new Ticket</h2>
+
+      <CreateForm />
+    </main>
+  );
+};
+
+export default CreateTicket;
