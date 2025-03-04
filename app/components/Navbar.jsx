0a05dfe6556cc1653939d1d0d8e3c26a12f8ea3d
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2025-03-04 10:38:03 +0000

    added : styles,fonts and image

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Logo from '../../public/logo.png'
+
+export default function Navbar() {
+  return (
+    <nav>
+      
+      <Image
+        src={Logo}
+        alt='logo'
+        width={70}
+        placeholder='blur'
+        quality={100}
+      />
+
+
+      <h1>Intro Next App</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
+  )
+}
